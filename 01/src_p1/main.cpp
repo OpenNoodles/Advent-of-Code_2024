df6831d9cad99a41094ef436da6cbfd7c0e1ab43
Kacu <kacper.kaleta@scythe-studio.com>
2024-12-02 00:02:19 +0100

    uploaded day01 data and lazy solution

--- 01/src_p1/main.cpp
+++ 01/src_p1/main.cpp
@@ -1,7 +1,27 @@
 #include "tools/ReadData.h"
-
+#include <iostream>
+#include <algorithm>
 
 int main() {
+    const int expectedDataSize = 1001;
+    std::vector<int> a, b;
+    a.reserve(expectedDataSize);
+    b.reserve(expectedDataSize);
+    ReadData::toVector<int>(a, b);
+    
+    long long result = 0;
+    std::sort(a.begin(), a.end());
+    std::sort(b.begin(), b.end());
+
+    for (size_t i = 0; i < a.size(); ++i) {
+        int value = a[i] - b[i];
+        if (value < 0) {
+            value = -value;
+        }
+        result += value;
+    }
+    std::cerr << result << "\n";
+    // Good Result: 1579939
 
     return 0;
 }
--- 01/src_p2/main.cpp
+++ 01/src_p2/main.cpp
@@ -1,7 +1,35 @@
 #include "tools/ReadData.h"
-
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <map>
 
 int main() {
+    const int expectedDataSize = 1001;
+    std::vector<int> a, b;
+    a.reserve(expectedDataSize);
+    b.reserve(expectedDataSize);
+    ReadData::toVector<int>(a, b);
+    
+    std::map<int, int> mapB;
+
+    for (const auto key : a) {
+        mapB[key] = 0;
+    }
+    for (const auto key : b) {
+        if (mapB.find(key) != mapB.end()) {
+            ++mapB[key];
+        }
+    }
+
+    long long similarityScore = 0;
+    for (const auto value : a) {
+        if (mapB.find(value) != mapB.end()) {
+            similarityScore += (value * mapB[value]);
+        }
+    }
+    std::cerr << similarityScore << "\n";
+    // Good Result: 20351745
 
     return 0;
 }
