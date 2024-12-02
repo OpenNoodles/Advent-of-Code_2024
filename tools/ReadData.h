30114bf1d642647a8d41ed2dbd7aecd938cdc127
Kacu <kacper.kaleta@scythe-studio.com>
2024-12-02 20:28:22 +0100

    implemented toVectorOfInputs operation in ReadData

--- tools/ReadData.h
+++ tools/ReadData.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <utility>
 #include <string>
 #include <vector>
@@ -18,6 +19,24 @@ void toVector(std::vector<DataType>& a, std::vector<DataType>& b) {
     while (inputFile >> valueA >> valueB) {
         a.emplace_back(valueA);
         b.emplace_back(valueB);
+    }
+}
+
+template <class DataType>
+void toVectorOfInputs(std::vector<std::vector<DataType>>& vec) {
+    std::ifstream inputFile("./data");
+    if (!inputFile.is_open()) {
+        throw std::runtime_error("ReadData::toVectorOfInputs: Can't find file");
+    }
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        std::vector<DataType> row;
+        std::istringstream stream(line);
+        DataType value;
+        while (stream >> value) {
+            row.push_back(value);
+        }
+        vec.push_back(row);
     }
 }
 
