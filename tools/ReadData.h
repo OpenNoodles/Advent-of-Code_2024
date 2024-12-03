4ac0f332715afb0e506044a87ec24076d59b0edf
Kacu <190228982+OpenNoodles@users.noreply.github.com>
2024-12-03 17:52:08 +0100

    added toString operation in ReadData file

--- tools/ReadData.h
+++ tools/ReadData.h
@@ -9,25 +9,30 @@
 
 namespace ReadData 
 {
+std::ifstream openFile();
+
+std::string toString() {
+    auto inputFile = openFile();
+    std::stringstream buffer;
+    buffer << inputFile.rdbuf();
+    inputFile.close();
+    return buffer.str();
+}
+
 template <class DataType>
 void toVector(std::vector<DataType>& a, std::vector<DataType>& b) {
-    std::ifstream inputFile("./data");
-    if (!inputFile.is_open()) {
-        throw std::runtime_error("ReadData::toVector: Can't find file");
-    }
+    auto inputFile = openFile();
     DataType valueA, valueB;
     while (inputFile >> valueA >> valueB) {
         a.emplace_back(valueA);
         b.emplace_back(valueB);
     }
+    inputFile.close();
 }
 
 template <class DataType>
 void toVectorOfInputs(std::vector<std::vector<DataType>>& vec) {
-    std::ifstream inputFile("./data");
-    if (!inputFile.is_open()) {
-        throw std::runtime_error("ReadData::toVectorOfInputs: Can't find file");
-    }
+    auto inputFile = openFile();
     std::string line;
     while (std::getline(inputFile, line)) {
         std::vector<DataType> row;
@@ -38,6 +43,15 @@ void toVectorOfInputs(std::vector<std::vector<DataType>>& vec) {
         }
         vec.push_back(row);
     }
+    inputFile.close();
+}
+
+std::ifstream openFile() {
+    std::ifstream inputFile("./data");
+    if (!inputFile.is_open()) {
+        throw std::runtime_error("ReadData::openFile: Can't find file");
+    }
+    return inputFile;
 }
 
 }
