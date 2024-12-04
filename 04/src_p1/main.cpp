c8c1ac3163c7eae03f48525346c62c474430c5ea
Kacu <190228982+OpenNoodles@users.noreply.github.com>
2024-12-04 21:15:26 +0100

    uploaded day04, the task, data, and lazy implementation

--- 04/src_p1/main.cpp
+++ 04/src_p1/main.cpp
@@ -1,7 +1,156 @@
 #include "tools/ReadData.h"
 
 
-int main() {
+void processWordSearch(const std::vector<std::vector<char>>& data);
 
+int searchHorizontal(const std::vector<std::vector<char>>& data);
+int searchVertical(const std::vector<std::vector<char>>& data);
+int searchDiagonalRight(const std::vector<std::vector<char>>& data);
+int searchDiagonalLeft(const std::vector<std::vector<char>>& data);
+
+std::string getHorizontalSlice(const std::vector<char>& row);
+std::string getVerticalSlice(const std::vector<std::vector<char>>& data, const size_t rowIndex);
+std::string getDiagonalRightSlice(const std::vector<std::vector<char>>& data, const size_t startX, const size_t startY);
+std::string getDiagonalLeftSlice(const std::vector<std::vector<char>>& data, const size_t startX, const size_t startY);
+
+int countOccurrence(const std::string& slice, const std::string& word);
+int countInSlice(const std::string& slice);
+
+int main() {
+    std::vector<std::vector<char>> data;
+    data.reserve(140);
+    ReadData::toMatrix<char>(data);
+    processWordSearch(data);
+    // RESULT: Your puzzle answer was 2549
     return 0;
+}
+
+void processWordSearch(const std::vector<std::vector<char>>& data) {
+    int count = 0;
+    count += searchHorizontal(data); // 406
+    count += searchVertical(data); // 424
+    count += searchDiagonalRight(data); // 876
+    count += searchDiagonalLeft(data); // 857
+    // std::cerr << "Horizontal: " << searchHorizontal(data) << "\n";
+    // std::cerr << "Vertical: " << searchVertical(data) << "\n";
+    // std::cerr << "DiagonalRight: " << searchDiagonalRight(data) << "\n";
+    // std::cerr << "DiagonalLeft: " << searchDiagonalLeft(data) << "\n";
+    std::cerr << count << "\n";
+}
+
+int searchHorizontal(const std::vector<std::vector<char>>& data) {
+    int count = 0;
+    for (size_t i = 0; i < data.size(); ++i) {
+        const auto slice = getHorizontalSlice(data[i]);
+        count += countInSlice(slice);
+    }
+    return count;
+}
+
+int searchVertical(const std::vector<std::vector<char>>& data) {
+    int count = 0;
+    const size_t numberOfColumns = data[0].size();
+
+    for (size_t i = 0; i < numberOfColumns; ++i) {
+        const auto slice = getVerticalSlice(data, i);
+        count += countInSlice(slice);
+    }
+    return count;
+}
+
+int searchDiagonalRight(const std::vector<std::vector<char>>& data) {
+    int count = 0;
+    const size_t numberOfRows = data.size();
+    const int numberOfColumns = static_cast<int>(data[0].size());
+
+    // std::cerr << "\n\n";
+
+    for (int startX = numberOfColumns - 1; startX >= 0; --startX) {
+        const auto slice = getDiagonalRightSlice(data, static_cast<size_t>(startX), 0);
+        count += countInSlice(slice);
+        // std::cerr << slice << "  " << countInSlice(slice) << "\n";
+    }
+    for (size_t startY = 1; startY < numberOfRows; ++startY) {
+        const auto slice = getDiagonalRightSlice(data, 0, startY);
+        count += countInSlice(slice);
+        // std::cerr << slice << "  " << countInSlice(slice) << "\n";
+    }
+    return count;
+}
+
+int searchDiagonalLeft(const std::vector<std::vector<char>>& data) {
+    int count = 0;
+    const size_t numberOfRows = data.size();
+    const size_t numberOfColumns = data[0].size();
+
+    for (size_t startX = 0; startX < numberOfColumns; ++startX) {
+        const auto slice = getDiagonalLeftSlice(data, startX, 0);
+        count += countInSlice(slice);
+    }
+    for (size_t startY = 1; startY < numberOfRows; ++startY) {
+        const auto slice = getDiagonalLeftSlice(data, numberOfColumns - 1, startY);
+        count += countInSlice(slice);
+    }
+    return count;
+}
+
+std::string getHorizontalSlice(const std::vector<char>& row) {
+    std::string slice;
+    for (const auto& value : row) {
+        slice += value;    
+    }   
+    return slice;
+}
+
+std::string getVerticalSlice(const std::vector<std::vector<char>>& data, const size_t rowIndex) {
+    std::string slice;
+    const size_t numberOfRows = data.size();
+    for (size_t i = 0; i < numberOfRows; ++i) {
+        slice += data[i][rowIndex]; 
+    } 
+    return slice;
+}
+
+std::string getDiagonalRightSlice(const std::vector<std::vector<char>>& data, const size_t startX, const size_t startY) {
+    std::string slice;
+    const size_t numberOfRows = data.size();
+    const size_t numberOfColumns = data[0].size();
+    size_t x = startX;
+    size_t y = startY;
+    while (x < numberOfColumns && y < numberOfRows) {
+        slice += data[x][y];
+        ++x;
+        ++y;
+    }
+    return slice;
+}
+
+std::string getDiagonalLeftSlice(const std::vector<std::vector<char>>& data, const size_t startX, const size_t startY) {
+    std::string slice;
+    const int numberOfRows = static_cast<int>(data.size());
+    int x = static_cast<int>(startX);
+    int y = static_cast<int>(startY);
+    while (x >= 0 && y < numberOfRows) {
+        slice += data[x][y];
+        --x;
+        ++y;
+    }
+    return slice;
+}
+
+int countInSlice(const std::string& slice) {
+    int count = 0;
+    count += countOccurrence(slice, "XMAS");
+    count += countOccurrence(slice, "SAMX");
+    return count;
+}
+
+int countOccurrence(const std::string& slice, const std::string& word) {
+    size_t i = slice.find(word);
+    int count = 0;
+    while (i < std::string::npos) {
+        ++count;
+        i = slice.find(word, i + word.size());
+    }
+    return count;
 }
--- 04/src_p2/main.cpp
+++ 04/src_p2/main.cpp
@@ -1,7 +1,74 @@
 #include "tools/ReadData.h"
 
 
-int main() {
+void processWordSearch(const std::vector<std::vector<char>>& data);
 
+int processChunk(const std::vector<std::vector<char>>& data, const size_t startX, const size_t startY);
+bool verifyChunkSize(const std::vector<std::vector<char>>& data, const size_t startX, const size_t startY);
+bool checkSlice(const std::string& slice);
+
+int main() {
+    std::vector<std::vector<char>> data;
+    data.reserve(140);
+    ReadData::toMatrix<char>(data);
+    processWordSearch(data);
+    // RESULT: Your puzzle answer was
     return 0;
+}
+
+void processWordSearch(const std::vector<std::vector<char>>& data) {
+    int count = 0;
+    const size_t numberOfRows = data.size();
+    const size_t numberOfColumns = data[0].size();
+    for (size_t x = 0; x < numberOfColumns; ++x) {
+        for (size_t y = 0; y < numberOfRows; ++y) {
+            count += processChunk(data, x, y);
+        }
+    }
+    std::cerr << count << "\n";
+}
+
+int processChunk(const std::vector<std::vector<char>>& data, const size_t startX, const size_t startY) {
+    if (!verifyChunkSize(data, startX, startY)) {
+        return 0;
+    }
+    std::string slice_1;
+    slice_1 += data[startX][startY];
+    slice_1 += data[startX + 1][startY + 1];
+    slice_1 += data[startX + 2][startY + 2];
+    if (!checkSlice(slice_1)) {
+        return 0;
+    }
+    std::string slice_2;
+    slice_2 += data[startX][startY + 2];
+    slice_2 += data[startX + 1][startY + 1];
+    slice_2 += data[startX + 2][startY];
+    if (!checkSlice(slice_2)) {
+        return 0;
+    }
+    return 1;
+}
+
+bool verifyChunkSize(const std::vector<std::vector<char>>& data, const size_t startX, const size_t startY) {
+    const size_t chunkSize = 2;
+    if ((startX + chunkSize) >= data[0].size()) {
+        return false;
+    }
+    if ((startY + chunkSize) >= data.size()) {
+        return false;
+    }
+    return true;
+}
+
+bool checkSlice(const std::string& slice) {
+    if (slice.size() != 3) {
+        throw "";
+    }
+    if (slice == "MAS") {
+        return true;
+    }
+    if (slice == "SAM") {
+        return true;
+    }
+    return false;
 }
