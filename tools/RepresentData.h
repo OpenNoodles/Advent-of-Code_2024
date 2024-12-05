0cfd55f22347c6890d274a57c3f1ad9abfa78cf7
Kacu <190228982+OpenNoodles@users.noreply.github.com>
2024-12-05 09:16:11 +0100

    created new operation for data representation

--- tools/RepresentData.h
+++ tools/RepresentData.h
@@ -35,8 +35,55 @@ void toVector(const Section& section, std::vector<std::pair<DataType, DataType>>
     }
 }
 
+
 template <class DataType>
-void toVector(const Section& section, std::vector<int>& vec, const std::string& delimiter = " ") {
+void toVector(const Section& section, std::vector<std::vector<DataType>>& vec, const std::string& delimiter = " ") {
+    for (const auto& line : section) {
+        vec.push_back(std::vector<DataType>());
+        if (delimiter == " ") {
+            std::istringstream stream(line);
+            DataType value;
+            while (stream >> value) {
+                vec.back().push_back(value);
+            }
+            continue;
+        }
+        size_t tail = 0;
+        size_t index = line.find(delimiter);
+        const size_t limit = line.size();
+        while (index < limit) {
+            std::istringstream stream(line.substr(tail, index - tail));
+            DataType value;
+            stream >> value;
+            vec.back().push_back(value);
+            tail = index + delimiter.size();
+            index = line.find(delimiter, index + 1);
+        }
+        std::istringstream stream(line.substr(tail));
+        DataType value;
+        stream >> value;
+        vec.back().push_back(value);
+
+        // const auto delimiterPos = line.find(delimiter);
+        // if (delimiterPos == std::string::npos || delimiterPos >= line.size()) {
+        //     throw std::invalid_argument("Invalid line format: " + line);
+        // }
+
+
+        // std::istringstream stream(line.substr(0, delimiterPos));
+        // streamA >> value;
+        // vec.push_back(value);
+        // std::cerr << vec.back().first << "  " << vec.back().second << "\n";
+    }
+}
+
+
+
+
+
+
+template <class DataType>
+void toVector(const Section& section, std::vector<DataType>& vec, const std::string& delimiter = " ") {
     for (const auto& line : section) {
 
         if (delimiter == " ") {
